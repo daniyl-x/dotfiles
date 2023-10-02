3739d66fd3cdd361488a49f1beb1e26805e6672b
daniyl-x <daniyl-x@proton.me>
2023-10-02 14:52:46 +0300

    added everforest theme to qtile

--- .config/qtile/colors.py
+++ .config/qtile/colors.py
@@ -1,3 +1,23 @@
+everforest = [
+    "#232a2e", # color0
+    "#e67e80", # color1
+    "#a7c080", # color2
+    "#e69875", # color3
+    "#7fbbb3", # color4
+    "#d699b6", # color5
+    "#83c092", # color6
+    "#d3c6aa", # color7
+
+    "#2d353b", # color8
+    "#e67e80", # color9
+    "#a7c080", # color10
+    "#e69875", # color11
+    "#7fbbb3", # color12
+    "#d699b6", # color13
+    "#83c092", # color14
+    "#d3c6aa", # color15
+]
+
 palenight = [
     "#0f111a", # color0
     "#f07178", # color1
@@ -7,6 +27,7 @@ palenight = [
     "#c792ea", # color5
     "#89ddff", # color6
     "#acafb9", # color7
+
     "#292d3e", # color8
     "#ff8b92", # color9
     "#ddffa7", # color10
