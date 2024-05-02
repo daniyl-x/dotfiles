6052d659f33c4f67ac6a93ab7806526522634d6b
daniyl-x <daniyl-x@proton.me>
2024-05-02 17:59:13 +0300

    Set <leader>f for lsp formatting nvim

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -24,6 +24,7 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', '<C-h>', vim.lsp.buf.signature_help, bufopts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 end
 
