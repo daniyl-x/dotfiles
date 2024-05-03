888874d06e29254482e0df7e701ed210c03ac77d
daniyl-x <daniyl-x@proton.me>
2024-05-03 21:33:51 +0300

    Fix python formatting

--- .config/qtile/colors.py
+++ .config/qtile/colors.py
@@ -1,80 +1,79 @@
 doom_one = [
-    "#282c34", # color0
-    "#ff6c6b", # color1
-    "#799033", # color2
-    "#da8548", # color3
-    "#2257a0", # color4
-    "#c678dd", # color5
-    "#5699af", # color6
-    "#bbc2cf", # color7
+    "#282c34",  # color0
+    "#ff6c6b",  # color1
+    "#799033",  # color2
+    "#da8548",  # color3
+    "#2257a0",  # color4
+    "#c678dd",  # color5
+    "#5699af",  # color6
+    "#bbc2cf",  # color7
 
-    "#4b5158", # color8
-    "#da8548", # color9
-    "#98be65", # color10
-    "#ecbe7b", # color11
-    "#51afef", # color12
-    "#a9a1e1", # color13
-    "#46d9ff", # color14
-    "#efefef", # color15
+    "#4b5158",  # color8
+    "#da8548",  # color9
+    "#98be65",  # color10
+    "#ecbe7b",  # color11
+    "#51afef",  # color12
+    "#a9a1e1",  # color13
+    "#46d9ff",  # color14
+    "#efefef",  # color15
 ]
 
 everforest = [
-    "#232a2e", # color0
-    "#e67e80", # color1
-    "#a7c080", # color2
-    "#e69875", # color3
-    "#7fbbb3", # color4
-    "#d699b6", # color5
-    "#83c092", # color6
-    "#d3c6aa", # color7
+    "#232a2e",  # color0
+    "#e67e80",  # color1
+    "#a7c080",  # color2
+    "#e69875",  # color3
+    "#7fbbb3",  # color4
+    "#d699b6",  # color5
+    "#83c092",  # color6
+    "#d3c6aa",  # color7
 
-    "#7a8478", # color8
-    "#e67e80", # color9
-    "#a7c080", # color10
-    "#e69875", # color11
-    "#7fbbb3", # color12
-    "#d699b6", # color13
-    "#83c092", # color14
-    "#d3c6aa", # color15
+    "#7a8478",  # color8
+    "#e67e80",  # color9
+    "#a7c080",  # color10
+    "#e69875",  # color11
+    "#7fbbb3",  # color12
+    "#d699b6",  # color13
+    "#83c092",  # color14
+    "#d3c6aa",  # color15
 ]
 
 one_dark = [
-    "#21262d", # color0
-    "#e06c75", # color1
-    "#98c379", # color2
-    "#e5c07b", # color3
-    "#61afef", # color4
-    "#c678dd", # color5
-    "#56b6c2", # color6
-    "#abb2bf", # color7
+    "#21262d",  # color0
+    "#e06c75",  # color1
+    "#98c379",  # color2
+    "#e5c07b",  # color3
+    "#61afef",  # color4
+    "#c678dd",  # color5
+    "#56b6c2",  # color6
+    "#abb2bf",  # color7
 
-    "#5c6370", # color8
-    "#e06c75", # color9
-    "#98c379", # color10
-    "#e5c07b", # color11
-    "#61afef", # color12
-    "#c678dd", # color13
-    "#56b6c2", # color14
-    "#efefef", # color15
+    "#5c6370",  # color8
+    "#e06c75",  # color9
+    "#98c379",  # color10
+    "#e5c07b",  # color11
+    "#61afef",  # color12
+    "#c678dd",  # color13
+    "#56b6c2",  # color14
+    "#efefef",  # color15
 ]
 
 palenight = [
-    "#0f111a", # color0
-    "#f07178", # color1
-    "#c3e88d", # color2
-    "#ffcb6b", # color3
-    "#82aaff", # color4
-    "#c792ea", # color5
-    "#89ddff", # color6
-    "#acafb9", # color7
+    "#0f111a",  # color0
+    "#f07178",  # color1
+    "#c3e88d",  # color2
+    "#ffcb6b",  # color3
+    "#82aaff",  # color4
+    "#c792ea",  # color5
+    "#89ddff",  # color6
+    "#acafb9",  # color7
 
-    "#676e95", # color8
-    "#ff8b92", # color9
-    "#ddffa7", # color10
-    "#ffe585", # color11
-    "#9cc4ff", # color12
-    "#e1acff", # color13
-    "#a3f7ff", # color14
-    "#ffffff", # color15
+    "#676e95",  # color8
+    "#ff8b92",  # color9
+    "#ddffa7",  # color10
+    "#ffe585",  # color11
+    "#9cc4ff",  # color12
+    "#e1acff",  # color13
+    "#a3f7ff",  # color14
+    "#ffffff",  # color15
 ]
-
