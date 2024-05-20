7aaf06d02e924957bdb4eb41d8c057ba4646958f
daniyl-x <daniyl-x@proton.me>
2024-05-20 19:22:34 +0300

    Disable line wrapping in nvim

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -37,4 +37,5 @@ vim.opt.colorcolumn = "80"          -- vertical line on 80 column
 vim.opt.cursorline = true           -- highlight current line
 vim.opt.termguicolors = true        -- correct colors
 vim.opt.linebreak = true            -- don't break words when wrapping lines
+vim.opt.wrap = false                -- disable line wrapping
 
